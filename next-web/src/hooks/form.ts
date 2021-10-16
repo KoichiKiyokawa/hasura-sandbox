92a7951dd57b336e38abdcd6b8071bb8859b3190
K.Kiyokawa <koichi20110068@gmail.com>
2021-10-17 00:31:43 +0900

    chore: add comment

--- next-web/src/hooks/form.ts
+++ next-web/src/hooks/form.ts
@@ -1,5 +1,13 @@
 import { useCallback, useState } from "react"
 
+/**
+ * form要素にnameをつけることで，フォーム値の更新を良い感じにできるようにする
+ * @example
+ * const { form, handleFormChange } = useForm({ first: '', last: '' })
+ * // 略
+ * <input name="first" onChange={handleFormChange} />
+ * <input name="last" onChange={handleFormChange} />
+ */
 export const useForm = <T>(initialValue: T) => {
   const [form, setForm] = useState(initialValue)
   const handleFormChange: React.ChangeEventHandler<HTMLInputElement> =
